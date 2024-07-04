cd5b5970dc251e8ff4e0411df31ce04960dea526
Alexrey003 <alexrey003lol@gmail.com>
2024-07-03 19:01:11 -0600

    Agregando el primer archivo al repositorio local llamado hola mundo.py

---
+++ 1_hola_mundo.py
@@ -0,0 +1,15 @@
+"""Para poder imprimir algo en pantalla en Python es tan facil con pasar la funcion
+print("Aqui va lo que quieres imprimir")
+Esto hara que se imprimia lo que le pasamos en consola"""
+
+#Imprimiendo texto
+print("Hola mundo")
+
+#Imprimiendo enteros
+print(60)
+
+#Imprimiendo flotantes
+print(67.34)
+
+#Imprimiendo booleanos
+print(True)
