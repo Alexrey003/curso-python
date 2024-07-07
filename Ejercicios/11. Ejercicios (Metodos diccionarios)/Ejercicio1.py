a39a24d07c70ec3f5344cd53cb02ce39d99b8672
Alexrey003 <alexrey003lol@gmail.com>
2024-07-07 14:30:07 -0600

    Se agrego el ejercicio del tema: Metodos de diccionarios

---
+++ Ejercicios/11. Ejercicios (Metodos diccionarios)/Ejercicio1.py
@@ -0,0 +1,14 @@
+'''Metodos diccionarios
+
+Temas a reforzar en esta práctica:
+- Inputs
+- Prints
+- Metodos diccionarios
+- Diccionarios
+- variables
+
+Ejercicio:
+Crea tu propio diccionario del tema que quieras y realiza un dir para ver que puedes hacer con los diccionarios y pon a prueba cada uno de
+los métodos disponibles.
+
+Imprime en pantalla los resultados que obtengas'''
